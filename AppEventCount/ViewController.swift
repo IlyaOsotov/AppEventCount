2f5c94b4633b831ba5680f5c9f10b0f728003b9e
IlyaOsotov <soten58@yandex.ru>
2021-09-08 18:45:12 +0700

    Initial Commit

---
+++ AppEventCount/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppEventCount
+//
+//  Created by Илья Осотов on 08.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
